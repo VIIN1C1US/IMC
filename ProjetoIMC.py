068a2992da9ebac1c8f70d044aaf3b6b97bf7dd4
Vinicius Souza <vinicokie@gmail.com>
2025-02-18 13:29:51 -0300

    IMC 2

--- ProjetoIMC.py
+++ ProjetoIMC.py
@@ -1,22 +1,19 @@
-def calcular_imc(peso, altura):
-    imc = peso / (altura **2)
-
-    if imc < 18.5:
-        return f"seu IMC é {imc:.2f}. classificação: baixo peso."
-    elif 18.5 <= imc < 24.9:
-        return f"Seu IMC é {imc:.2f}. classificação: peso adequado."
-    elif 25 <= imc < 29.9:
-        return f"seu IMC é {imc:.2f}. classificação: sobrepeso."
-    elif 30 <= imc < 34.9:
-        return f"seu IMC é {imc:.2f}. classificação: Obessidade Grau 1."
-    elif 35 <= imc < 39.9:
-        return f"seu IMC é {imc:.2f}. classificação: Obessidade Grau 2."
-    else:
-        return f"seu IMC é {imc:.2f}. classificação: Obessidade Grau 3."
 peso = float(input("digite seu peso em kg: "))
-
 altura = float(input("digite sua altura em metros: "))
 
-resultado = calcular_imc(peso, altura)
+imc = peso / (altura ** 2)
 
-print(resultado)
+if imc < 18.5:
+    classificacao = "baixo peso"
+elif imc < 24.9:
+    classificacao = "peso adequado"
+elif imc < 29.9:
+    classificacao = "sobrepeso"
+elif imc < 34.9:
+    classificacao = "obesidade grau 1"
+elif imc < 39.9:
+    classificacao = "obesidade grau 2"
+else:
+    classificacao = "obesidade grau 3"
+
+print(f"seu IMC é {imc:.2f}. calssificação: {classificacao}")
